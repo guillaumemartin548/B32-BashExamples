838cdee09886a6c47be8918ca2a2a8f0e8fb619f
Guillaume Martin <guillaumemartin548@gmail.com>
2018-12-03 11:17:12 -0500

    fichiers terminé

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+if test -e $1
+then
+	echo "existe"
+	if test -d $1 #-d si c'est un directory
+	then
+		echo "Il est un répertoire"
+	fi
+else
+	echo "inexistant"
+fi
+
+#vérifie si le dossier ou fichier inscrit est existant
+
+
