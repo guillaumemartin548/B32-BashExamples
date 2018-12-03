49c72c6659f349771d5b12d7efe421a6569e5c71
Guillaume Martin <guillaumemartin548@gmail.com>
2018-12-03 11:10:21 -0500

    08 fait

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+echo -n "Texte : "
+read texte
+
+if [[ $texte =~ ^[0-9]+$ ]];then #rien avant et rien après, le + fait que le même chaîne peut revenir plus d'une fois
+		echo "Numérique"
+fi
+
+# le ~ permet de faire comprendre au système qu'on cherche une expression régulière comme grep
