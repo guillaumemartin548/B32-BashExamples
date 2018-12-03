a67c70510dd34dc104c1a03f86d5223450853ad6
Guillaume Martin <guillaumemartin548@gmail.com>
2018-12-03 10:03:55 -0500

    02 complete

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
 
+prenom=Guillaume #il ne doit pas y avoir d'espaces avec les = pour les assignations, pas de $ pour la variable 
+# il doit y avoir des guillemets s'il y a des espaces pour le contenu de la variable
+echo $prenom
+
+#Forcer une declaration d'une variable int
+declare -i age=33
+
+#Mettre une variable  Read only
+declare -r age2=33
+
+# Les variables système 
+# des variables qui existe et qu'on utilise, mais à ne pas modifier
+
+#sort le nom de l'exécutable
+echo $0
+
+#premier argument quand tu appel le script et ainsi de suite jusqu'à 9 
+echo $1
+echo $2
+
+#pour savoir le nombre d'arguments entrés
+echo $#
+
+
+
