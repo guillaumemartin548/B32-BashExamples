cd7c8565e80cf0a7bc8e1a3303b61f616465b03a
Guillaume Martin <guillaumemartin548@gmail.com>
2018-12-03 10:09:16 -0500

    03 terminé

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+echo 10+19
+
+echo $((10+19))
+#pour exécuter ça prend le signe de $ plus les doubles  parenthèses
+
+read -p "Nombre 1 : " premier
+read -p "Nombre 2: " second
+
+let resultat=$premier*$second
+#le let permet de ne pas avoir à ajouter les parenthèses et le signe $
+
+echo $resultat 
