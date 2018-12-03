a0316dd2a4c84ff5b3187b92eeb3e6f42d4766ac
Guillaume Martin <guillaumemartin548@gmail.com>
2018-12-03 10:52:08 -0500

    05 terminé

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+	echo $1 
+done
+
+liste=`ls`
+
+#pour chacun des éléments de la liste fais...
+for fic in $liste  #comme un for each en powershell, va chercher chaque élément dans la variable 
+do
+	echo $fic
+done
+
+
+
+resultat=o
+while test $resultat = o -o $resultat = O #-o = || en C -a && en C
+do 
+	read -p "Entrez o pour répéter la boucle: " resultat
+done
+
