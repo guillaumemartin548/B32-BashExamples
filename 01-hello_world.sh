db1c3c9cde156d369ea15bde4b7a5280f522fd03
Guillaume Martin <guillaumemartin548@gmail.com>
2018-12-03 09:49:48 -0500

    hello_world complete

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+# le signe pour les commentaires
+
+echo "Salut"
+
+echo -n "Vive "    #-n empêche le changement de ligne
+echo "Linux"
+
+echo -e  "123\n123\n123"  # -e permet d'utiliser les caractères spéciaux comme \t et \n comme en c++
+
+read -p "Vous avez quel âge?" age    #read comme un cin en c++, ce qu'on rentre va aller dans la variable age
+echo $age "ans! Vous paraissez plus jeune" #pour utiliser une variable assignée sans $ on utilise le signe $
+
